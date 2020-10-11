920c9f675f4b7f35c61a6948609c0ec7de532679
Patrik Kjærran <patrikkja@gmail.com>
2020-10-11 19:18:06 +0200

    Configure project to be runnable in local JupyterLab instance

---
+++ run_server.cmd
@@ -0,0 +1,1 @@
+venv\Scripts\activate & start chrome --app=http://localhost:8888/lab & jupyter lab
---
+++ setup.bat
@@ -0,0 +1,5 @@
+python -m venv venv
+source ./venv/Scripts/activate
+pip install jupyterlab
+pip install -r requirements.txt
+deactivate
